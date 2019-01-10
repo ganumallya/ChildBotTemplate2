c7897d4fd8097fd72a4d99fe44c262171623f860
Harshitaaaa <43233069+Harshitaaaa@users.noreply.github.com>
2019-01-10 14:53:28 +0530

    Restrict Users

---
+++ Dialogs/restrictUser.js
@@ -0,0 +1,21 @@
+var builder = require('botbuilder');
+const {config} = require('../../Config/config');
+const { userList } = require('./../userList')
+
+module.exports = (bot) => {
+    bot.dialog('/RestrictUserDialog', [
+        function (session, args, next) {
+            session.endDialog("Het, this bot will come to you soon")
+        }
+    ]).triggerAction({
+        onFindAction:(session,callback)=>{
+            var userid = session.message.address.user.id;
+            if(userList.includes(userid)){
+                callback(null,0)
+            }
+            else{
+                callback(null,1)
+            }
+        }
+    });
+};
