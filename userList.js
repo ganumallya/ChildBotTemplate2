42ed932fa4a5778597b1f839e18bb1c31c7c8273
Harshitaaaa <43233069+Harshitaaaa@users.noreply.github.com>
2019-01-10 14:53:56 +0530

    Allowed users list

---
+++ userList.js
@@ -0,0 +1,3 @@
+var users = ['harshita.badlani@taoautomation.com','ravi@taoautomation.com','itsupport@taoautomation.com','kunal.mundada@taoautomation.com','akhilesh.yadav@taoautomation.com','amishi.dadhich@taoautomation.com','ganesh.mallya@taoautomation.com','kavyashree@taoautomation.com','mohammedshakeel.muchale@taoautomation.com','Pratheeksha.karanth@taoautomation.com','Raksha.Deshmukh@taoautomation.com','shanthaveera.hiremath@taoautomation.com','shivanand.belagali@taoautomation.com']
+
+module.exports = users
