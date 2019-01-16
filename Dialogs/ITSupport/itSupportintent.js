7d76ac54d158a58a8576c717446ad2fa8866feaf
Harshitaaaa <43233069+Harshitaaaa@users.noreply.github.com>
2019-01-16 13:52:25 +0530

    it support intent

--- Dialogs/ITSupport/itSupportintent.js
+++ Dialogs/ITSupport/itSupportintent.js
@@ -7,7 +7,7 @@ module.exports = (bot) => {
     bot.dialog('/ITIssuesDialog', [
         function (session, args, next) {
             var res = "I am here to help. Please type in your issue."
-            session.send(res)
+            session.endDialog(res)
             var id = session.message.address.user.id;
             require('../../mongoLog')(id, session.message.text, res, 'ITIssuesIntent')
         }
