1b3764cdf883bc08157a12bac2d343c30f26349e
Harshitaaaa <43233069+Harshitaaaa@users.noreply.github.com>
2019-01-16 13:40:28 +0530

    added it support intent

---
+++ Dialogs/ITSupport/itSupportintent.js
@@ -0,0 +1,18 @@
+
+var builder = require('botbuilder');
+const {config} = require('../../Config/config');
+
+module.exports = (bot) => {
+    
+    bot.dialog('/ITIssuesDialog', [
+        function (session, args, next) {
+            var res = "I am here to help. Please type in your issue."
+            session.send(res)
+            var id = session.message.address.user.id;
+            require('../../mongoLog')(id, session.message.text, res, 'ITIssuesIntent')
+        }
+    ]).triggerAction({
+        matches: 'ITIssuesIntent',
+        intentThreshold:config.CONFIDENCE_THRESHOLD
+    });
+};
